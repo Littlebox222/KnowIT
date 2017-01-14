257be41a5680791e410393c0247d337e7a119045
Littlebox222 <littlebox@LittledeiMac.local>
2017-01-15 02:53:04 +0800

    init structure

---
+++ KnowIT/KnowIT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KnowIT
+//
+//  Created by Little Box on 15/01/2017.
+//  Copyright © 2017 Little Box. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
